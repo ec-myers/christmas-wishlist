7161522d646c1cb8374decceb3cb6139221fe622
“Elyse <elysemyers1@gmail.com>
2019-10-17 17:09:54 -0600

    Adds functionality to display cards on the DOM

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let endDate = document.querySelector('#end-date');
 let type = document.querySelector('#type');
 let description = document.querySelector('#description');
 let btnSave = document.querySelector('#btn-save');
+let cardArea = document.querySelector('.card-area');
 
 formInputs.addEventListener('keyup', handleFormInputs);
 btnSave.addEventListener('click', addTrip);
@@ -17,8 +18,29 @@ function handleFormInputs(e) {
 }
 
 function addTrip() {
-  let newTrip = new Trip(Date.now(), destination.value, startDate.value, endDate.value, type.value, description.value);
+  let trip = new Trip(Date.now(), destination.value, startDate.value, endDate.value, type.value, description.value);
 
-  tripsArray.push(newTrip);
-  console.log(tripsArray)
+  tripsArray.push(trip);
+  trip.saveToStorage(tripsArray);
+  displayCard(trip);
+  btnSave.disabled = true;
+}
+
+function displayCard(trip) {
+  cardArea.insertAdjacentHTML('afterbegin', `<article class="trip-card" data-id="${trip.id}">
+        <header>
+          <h3 class="card-destination" contenteditable="true">${trip.destination}</h3>
+        </header>
+        <div class="card-content">
+         <div class="card-duration">
+          <p class="card-start-date" contenteditable="true">Start: ${trip.startDate}</p>
+          <p class="card-end-date" contenteditable="true">End: ${trip.endDate}</p>
+          </div>
+          <p class="card-type">Type: ${trip.type}</p>  
+          <p class="card-body" contenteditable="true">${trip.description}</p>
+        </div>
+        <footer>
+          <img class="img-delete" src="images/delete-button.svg" alt="delete" id="btn-delete">
+        </footer>
+      </article>`);
 }
